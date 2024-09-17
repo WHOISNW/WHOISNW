1cede5940319146a846110fc57d305d2229ddc54
WHOISNW <natthew1@outlook.com>
2024-09-17 02:26:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('active');
+    navbar.classList.toggle('active');
+});
